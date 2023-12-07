59b06b2591f6271bedd199aa05e612dd0d10d5f4
Roopa <roopa.konaparthi@gmail.com>
2023-12-07 09:17:31 +0530

    added simple interest code

--- first.py
+++ first.py
@@ -1,6 +1,13 @@
+'''
 print("This is my first program in python")
 a=12
 b=44
 c=a+b
 print("The sum of",a, "and",b, "is",c)
 print("This is end of the program")
+'''
+p=123455
+t=1
+r=2
+si=p*t*r/100
+print(si);
